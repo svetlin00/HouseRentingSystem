ce7a35d8ae072e5d512df65a0d8099ec1827b950
svetlin_ergeledjiev@abv.bg <svetlin_ergeledjiev@abv.bg>
2024-12-02 19:30:45 +0200

    Adding builder configuration

--- HouseRentingSystem.Data.Models/Models/ApplicationUser.cs
+++ HouseRentingSystem.Data.Models/Models/ApplicationUser.cs
@@ -5,5 +5,13 @@ namespace HouseRentingSystem.Data.Models.Models
 {
     public class ApplicationUser : IdentityUser<Guid>
     {
+        public ApplicationUser() 
+        {
+            this.RentedHouses = new HashSet<House>();
+        }
+
+        public ICollection<House> RentedHouses { get; set; }
     }
+
+    
 }
--- HouseRentingSystem.Data/Data/HouseRentingSystemDbContext.cs
+++ HouseRentingSystem.Data/Data/HouseRentingSystemDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace HouseRentingSystem.Data
 {
@@ -10,6 +11,20 @@ namespace HouseRentingSystem.Data
         public HouseRentingSystemDbContext(DbContextOptions<HouseRentingSystemDbContext> options)
             : base(options)
         {
+         
+        }
+
+        public DbSet<House> Houses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+       public DbSet<Agent> Agents { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            Assembly configAssembly = Assembly.GetAssembly(typeof(HouseRentingSystemDbContext))?? 
+            Assembly.GetExecutingAssembly();
+            builder.ApplyConfigurationsFromAssembly(configAssembly);
+            base.OnModelCreating(builder);
         }
     }
 }
